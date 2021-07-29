c3645b7a3189cf4a67f2de861d475b37cb8bc782
Simakeng <Simakeng@outlook.com>
2021-07-29 17:00:12 +0800

    能用的汇编代码生成

--- AssemblyGeneration.cpp
+++ AssemblyGeneration.cpp
@@ -1,43 +1,128 @@
 #include "AssemblyGeneration.h"
 
 #include <unordered_map>
+#include <sstream>
 
 using namespace std;
 
 
-namespace Compiler::ARM
+namespace Compiler::ASM
 {
-	unordered_map<Register, const char*> register_map = {
-		{Register::R1,"R1"},
-		{Register::R2,"R2"},
-		{Register::R3,"R3"},
-		{Register::R4,"R4"},
-		{Register::R5,"R5"},
-		{Register::R6,"R6"},
-		{Register::R7,"R7"},
-		{Register::R8,"R8"},
-		{Register::R9,"R9"},
-		{Register::R10,"R10"},
-		{Register::R11,"R11"},
-		{Register::R12,"R12"},
-		{Register::SP,"SP"},
-		{Register::LR,"LR"},
-		{Register::PC,"PC"}
+	unordered_map<Reg, const char*> register_map = {
+		{Reg::R0,"R0"},
+		{Reg::R1,"R1"},
+		{Reg::R2,"R2"},
+		{Reg::R3,"R3"},
+		{Reg::R4,"R4"},
+		{Reg::R5,"R5"},
+		{Reg::R6,"R6"},
+		{Reg::R7,"R7"},
+		{Reg::R8,"R8"},
+		{Reg::R9,"R9"},
+		{Reg::R10,"R10"},
+		{Reg::R11,"R11"},
+		{Reg::R12,"R12"},
+		{Reg::SP,"SP"},
+		{Reg::LR,"LR"},
+		{Reg::PC,"PC"}
 	};
 
-	string MakeAsm(const char* code, Register rd, Register rn, int imm)
+	std::string Make(const char* code, std::initializer_list<Reg> reglist)
+	{
+		stringstream ss;
+		ss << code << "\t{";
+		auto beg = reglist.begin();
+		for (int i = reglist.size(); i > 0; i--)
+		{
+			ss << register_map[*beg++];
+			if (i != 1)
+				ss << ", ";
+		}
+		ss << "}";
+		return ss.str();
+	}
+	string Make(const char* code, const char* operation)
 	{
 		//"code rd, rn"
-		return string(code) + "\t" + register_map[rd] + ", " + register_map[rn] + ", " + to_string(imm);
+		return string(code) + "\t" + operation;
 	}
-	string MakeAsm(const char* code, Register rd, Register rn)
+
+	string Make(const char* code, Reg rd, Reg rn, int imm)
+	{
+		//"code rd, rn"
+		return string(code) + "\t" + register_map[rd] + ", " + register_map[rn] + ", #" + to_string(imm);
+	}
+	string Make(const char* code, Reg rd, Reg rn)
 	{
 		return string(code) + "\t" + register_map[rd] + ", " + register_map[rn];
 	}
-	std::string MakeAsm(const char* code, Register rd, int imm)
+	std::string Make(const char* code, Reg rd, int imm)
 	{
 		return string(code) + "\t" + register_map[rd] + ", " + to_string(imm);
 	}
+
+	void AssemblyGenerator::AddPrologue(std::string text)
+	{
+		this->prologue_list.push_back(text);
+	}
+
+	void AssemblyGenerator::AddEpilogue(std::string text)
+	{
+		this->epilogue_list.push_back(text);
+	}
+
+	AssemblyGenerator::Section& AssemblyGenerator::CreateSection(const char* section_name)
+	{
+		section_list.emplace_back(move(Section(section_name)));
+		return section_list.back();
+	}
+
+	std::string AssemblyGenerator::Summary()
+	{
+		stringstream ss;
+
+		for (auto& prologues : prologue_list)
+			ss << prologues;
+
+		for (auto& sec : section_list)
+		{
+			ss << sec.Summary();
+			ss << "\n";
+		}
+
+		for (auto& epilogues : epilogue_list)
+			ss << epilogues;
+
+		return ss.str();
+	}
+	AssemblyGenerator::Section::Section(const char* name)
+	{
+		this->_name = string(name);
+	}
+
+	AssemblyGenerator::Section::Section(Section&& rhs) noexcept
+	{
+		this->code_list = std::move(rhs.code_list);
+		this->_name = std::move(rhs._name);
+	}
+
+	void AssemblyGenerator::Section::AddCode(std::string Code)
+	{
+		this->code_list.push_back(Code);
+	}
+
+	std::string AssemblyGenerator::Section::Summary()
+	{
+		stringstream ss;
+		ss << _name << ":\n";
+		for (auto& code_ : code_list)
+			ss << "\t" << code_ << "\n";
+		return ss.str();
+	}
+
+
+
+
 
 }
 
--- AssemblyGeneration.h
+++ AssemblyGeneration.h
@@ -3,7 +3,7 @@
 #include <deque>
 namespace Compiler
 {
-	namespace ARM
+	namespace ASM
 	{
 		class AssemblyGenerator
 		{
@@ -15,26 +15,30 @@ namespace Compiler
 			{
 			public:
 				~Section() = default;
+				Section(const Section&) = delete; // no copy
+				Section(Section&& rhs) noexcept; // can move
 			private:
 				Section(const char* name);
 				std::string _name;
+				std::deque<std::string> code_list;
 			public:
 				friend class AssemblyGenerator;
-
 				void AddCode(std::string Code);
 				std::string Summary();
 			};
+		public:
+			void AddPrologue(std::string text);
+			void AddEpilogue(std::string text);
+			Section& CreateSection(const char* section_name);
 		private:
 			std::deque<Section> section_list;
+			std::deque<std::string> prologue_list;
+			std::deque<std::string> epilogue_list;
 		public:
 			std::string Summary();
 		};
 
-		class BasicOperation
-		{
-		};
-
-		enum class Register
+		enum class Reg
 		{
 			R0 = 0,
 			R1,
@@ -57,100 +61,10 @@ namespace Compiler
 			PC = R15,
 		};
 
-		enum class OpCode
-		{
-			ADC,
-			ADD,
-			ADR,
-			ADRL,
-			AND,
-			ASR,
-			B,
-			BFC,
-			BFI,
-			BIC,
-			BKPT,
-			BL,
-			BLX,
-			BX,
-			BXJ,
-			CBZ,
-			CBNZ,
-			CDP,
-			CDP2,
-			CLREX,
-			CLZ,
-			CMP,
-			CMN,
-			DBG,
-			DMB,
-			DSB,
-			EOR,
-			ERET,
-			HVC,
-			ISB,
-			IT,
-			LDC,
-			LDC2,
-			LDM,
-			LDR,
-			LDREX,
-			LSL,
-			LSR,
-			MCR,
-			MCR2,
-			MCRR,
-			MCRR2,
-			MLA,
-			MLS,
-			MOV,
-			MOVT,
-			MRC,
-			MRC2,
-			MRRC,
-			MRRC2,
-			MRS,
-			MUL, MVN, NOP, ORN, ORR, PKHBT, PLD, PLI, POP, PUSH, QADD,
-			QADD8, QADD16, QASX, QDADD, QDSUB, QSAX, QSUB, QSUB8, QSUB16,
-			RBIT, REV, REV16, REVSH, RFE, ROR, RRX, RSB, RSC, SADD8, SADD16, SASX, SBC,
-			SBFX, SDIV, SEL, SETEND, SEV, SHADD7, SHADD16, SHASX, SHSAX, SHSUB8, SHSUB16,
-			SMC, SMLAxy, SMLAD, SMLAL, SMLALD, SMLALxy, SMLAWy, SMLSD, SMLSLD, SMMLA, SMMLS, SMMUL, SMUAD,
-			SMULxy,
-			SMULL, SMULWy, SMUSD, SRS, SSAT, SSAT16, SSAX, SSAT16, SSAX, SSUB8, SSUB16, STC, STC2, STM, STR,
-			STREX, SUB, SUBS, SVC, SWP, SWPB,
-			SXTAB,
-			SXTAB16,
-			SXTAH,
-			SYS,
-			TBB, TBH, TEQ, TST, UADD8, UADD16,
-			UASX, UBFX, UDIV, UHADD8, UHADD16,
-			UHASX, UHSAX, UHSUB8, UHSUB16,
-			UMAAL,
-			UMLAL,
-			UMULL,
-			UQADD8,
-			UQADD16,
-			UQASX,
-			UQSAX,
-			UQSUB8,
-			UQSUB16,
-			USAD8,
-			USADA8,
-			USAT,
-			USAT16,
-			USAX,
-			USUB8,
-			USUB16,
-			UXTAB,
-			UXTAB16,
-			UXTAH,
-			UXTB,
-			UXTB16,
-			UXTH, WFE, WFI, YIELD
-		};
-
-		std::string MakeAsm(const char* code, Register rd, Register rn, int imm);
-		std::string MakeAsm(const char* code, Register rd, Register rn);
-		std::string MakeAsm(const char* code, Register rd, int imm);
+		std::string Make(const char* code, const char* operation);
+		std::string Make(const char* code, std::initializer_list<Reg> reglist);
+		std::string Make(const char* code, Reg  rd, Reg rn, int imm);
+		std::string Make(const char* code, Reg  rd, Reg rn);
+		std::string Make(const char* code, Reg  rd, int imm);
 	}
 }
